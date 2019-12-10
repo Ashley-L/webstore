bfd68710f1baf1da5f958bba43751f78f7d8ae7c
Ashley-L <ashley.lall@outlook.com>
2019-12-10 04:17:22 -0500

    3 product filters + availability tag added.

--- js/index.js
+++ js/index.js
@@ -4,260 +4,260 @@ const products = [
 { //product 0
     image: `black-white-polka-dot.jpg`,
     name: `Polka Dots socks`,
-    buttonId: `faveButton0`,
+    // buttonId: `faveButton0`,
     originalPrice: 999.99,
     newPrice: 599.99,
     descrip: `Fight fashion faux-pas by bringing socks and sandals back into style.`,
-    style: `Dots`,
-    material: `Cotton`,
-    availability: `In stock`,
+    style: `dots`,
+    material: `cotton`,
+    availability: `in stock`,
     small: `s0`,
     medium: `m0`,
     large: `l0`,
 },{ //product 1
     image: `banana.jpg`,
     name: `Banana socks`,
-    buttonId: `faveButton1`,
+    // buttonId: `faveButton1`,
     originalPrice: 799.99,
     newPrice: 499.99,
     descrip: `We, personally, would not wear bananas on our socks, but we're sure someone might.`,
-    style: `Pattern`,
-    material: `Polyester`,
-    availability: `Low stock`,
+    style: `pattern`,
+    material: `polyester`,
+    availability: `low stock`,
     small: `s1`,
     medium: `m1`,
     large: `l1`,
 },{ //product 2
     image: `blue-orange.jpg`,
     name: `Abstract socks`,
-    buttonId: `faveButton2`,
+    // buttonId: `faveButton2`,
     originalPrice: 899.99,
     newPrice: 299.99,
-    style: `Dots`,
+    style: `pattern`,
     descrip: `These socks are so great that you'll still look good even if you're only wearing one of them.`,
-    material: `Wool`,
-    availability: `In stock`,
+    material: `wool`,
+    availability: `in stock`,
     small: `s2`,
     medium: `m2`,
     large: `l2`,
 },{ //product 3
     image: `orange-blue-polka-dot.jpg`,
     name: `Fake ladybug`,
-    buttonId: `faveButton3`,
+    // buttonId: `faveButton3`,
     originalPrice: 199.99,
     newPrice: 50.99,
     descrip: `These are some massive polka dots. It reminds us of a ladybug, except the  colours are completely wrong.`,
-    style: `Dots`,
-    material: `Polyester`,
-    availability: `Out of stock`,
+    style: `dots`,
+    material: `polyester`,
+    availability: `out of stock`,
     small: `s3`,
     medium: `m3`,
     large: `l3`,
 },{ //product 4
     image: `red-stripes.jpg`,
     name: `Candy cane socks`,
-    buttonId: `faveButton4`,
+    // buttonId: `faveButton4`,
     originalPrice: 499.99,
     newPrice: 399.99,
     descrip: `These socks are perfect for relaxing, as opposed to any other pair of socks.`,
-    style: `Stripes`,
-    material: `Cotton`,
-    availability: `In stock`,
+    style: `stripes`,
+    material: `cotton`,
+    availability: `in stock`,
     small: `s4`,
     medium: `m4`,
     large: `l4`,
 },{ //product 5
     image: `red-orange.jpg`,
     name: `Strawberry orange`,
-    buttonId: `faveButton5`,
+    // buttonId: `faveButton5`,
     originalPrice: 899.99,
     newPrice: 749.99,
     descrip: `Delicious.`,
-    style: `Stripes`,
-    material: `Polyester`,
-    availability: `Out of stock`,
+    style: `stripes`,
+    material: `polyester`,
+    availability: `out of stock`,
     small: `s5`,
     medium: `m5`,
     large: `l5`,
 },{ //product 6
     image: `classic-dots.jpg`,
     name: `Classic Dots`,
-    buttonId: `faveButton6`,
+    // buttonId: `faveButton6`,
     originalPrice: 199.99,
     newPrice: 99.99,
     descrip: `Perfect for all occasions.`,
-    style: `Dots`,
-    material: `Wool`,
-    availability: `Low stock`,
+    style: `dots`,
+    material: `wool`,
+    availability: `low stock`,
     small: `s6`,
     medium: `m6`,
     large: `l6`,
 },{ //product 7
     image: `bunny-socks.jpg`,
     name: `Bunny socks`,
-    buttonId: `faveButton7`,
+    // buttonId: `faveButton7`,
     originalPrice: 299.99,
     newPrice: 149.99,
     descrip: `Stay warm by wearing rabbits on your feet.`,
-    style: `Specialty`,
-    material: `Wool`,
-    availability: `In stock`,
+    style: `specialty`,
+    material: `wool`,
+    availability: `in stock`,
     small: `s7`,
     medium: `m7`,
     large: `l7`,
 },{ //product 8
     image: `yellow-polka-dot.jpg`,
     name: `Undercover socks`,
-    buttonId: `faveButton8`,
+    // buttonId: `faveButton8`,
     originalPrice: 499.99,
     newPrice: 399.99,
     descrip: `When you have an office job but still gotta look professional.`,
-    style: `Dots`,
-    material: `Cotton`,
-    availability: `Out of stock`,
+    style: `dots`,
+    material: `cotton`,
+    availability: `out of stock`,
     small: `s8`,
     medium: `m8`,
     large: `l8`,
 },{ //product 9
     image: `swearing.jpg`,
     name: `Um, rude`,
-    buttonId: `faveButton9`,
+    // buttonId: `faveButton9`,
     originalPrice: 999.99,
     newPrice: 699.99,
     descrip: `To all the people who annoy you.`,
-    style: `Specialty`,
-    material: `Polyester`,
-    availability: `Out of stock`,
+    style: `specialty`,
+    material: `polyester`,
+    availability: `out of stock`,
     small: `s9`,
     medium: `m9`,
     large: `l9`,
 },{ //product 10
     image: `striped-hearts.jpg`,
     name: `Striped hearts`,
-    buttonId: `faveButton10`,
+    // buttonId: `faveButton10`,
     originalPrice: 799.99,
     newPrice: 599.99,
     descrip: `When you can't decide between stripes or hearts.`,
-    style: `Specialty`,
-    material: `Cotton`,
-    availability: `In stock`,
+    style: `stripes`,
+    material: `cotton`,
+    availability: `in stock`,
     small: `s10`,
     medium: `m10`,
     large: `l10`,
 },{ //product 11
     image: `foot-glove.jpg`,
     name: `Foot glove`,
-    buttonId: `faveButton11`,
+    // buttonId: `faveButton11`,
     originalPrice: 1249.99,
     newPrice: 999.99,
     descrip: `Sometimes you want gloves, but for your feet.`,
-    style: `Specialty`,
-    material: `Cotton`,
-    availability: `Low stock`,
+    style: `specialty`,
+    material: `cotton`,
+    availability: `low stock`,
     small: `s11`,
     medium: `m11`,
     large: `l11`,
 },{ //product 12
     image: `girls-rule.jpg`,
     name: `Girls rule`,
-    buttonId: `faveButton12`,
+    // buttonId: `faveButton12`,
     originalPrice: 699.99,
     newPrice: 549.99,
     descrip: `Fight the patriarchy with your socks.`,
-    style: `Specialty`,
-    material: `Polyester`,
-    availability: `In stock`,
+    style: `specialty`,
+    material: `polyester`,
+    availability: `in stock`,
     small: `s12`,
     medium: `m12`,
     large: `l12`,
 },{ //product 13
     image: `doughnuts.jpg`,
     name: `Doughnuts`,
-    buttonId: `faveButton13`,
+    // buttonId: `faveButton13`,
     originalPrice: 199.99,
     newPrice: 29.99,
     descrip: `We love doughnuts, and these socks.`,
-    style: `Pattern`,
-    material: `Polyester`,
-    availability: `Out of stock`,
+    style: `pattern`,
+    material: `polyester`,
+    availability: `out of stock`,
     small: `s13`,
     medium: `m13`,
     large: `l13`,
 },{ //product 14
     image: `tall-vine.jpg`,
     name: `Weird stockings`,
-    buttonId: `faveButton14`,
+    // buttonId: `faveButton14`,
     originalPrice: 129.99,
     newPrice: 119.99,
     descrip: `Stay warm with these extra long socks.`,
-    style: `Specialty`,
-    material: `Wool`,
-    availability: `In stock`,
+    style: `specialty`,
+    material: `wool`,
+    availability: `in stock`,
     small: `s14`,
     medium: `m14`,
     large: `l14`,
 },{ //product 15
     image: `fuzzy-colourful.jpg`,
     name: `Rainbow`,
-    buttonId: `faveButton15`,
+    // buttonId: `faveButton15`,
     originalPrice: 1199.99,
     newPrice: 1099.99,
     descrip: `Extra warm, extra fuzzy, extra colourful.`,
-    style: `Specialty`,
-    material: `Wool`,
-    availability: `Out of stock`,
+    style: `specialty`,
+    material: `wool`,
+    availability: `out of stock`,
     small: `s15`,
     medium: `m15`,
     large: `l15`,
 },{ //product 16
     image: `abstract.jpg`,
     name: `Abstract`,
-    buttonId: `faveButton16`,
+    // buttonId: `faveButton16`,
     originalPrice: 499.99,
     newPrice: 379.99,
     descrip: `Show your artsy side with these strangely-patterned socks.`,
-    style: `Pattern`,
-    material: `Cotton`,
-    availability: `Low stock`,
+    style: `pattern`,
+    material: `cotton`,
+    availability: `low stock`,
     small: `s16`,
     medium: `m16`,
     large: `l16`,
 },{ //product 17
     image: `cassettes.jpg`,
     name: `Blast from the past`,
-    buttonId: `faveButton17`,
+    // buttonId: `faveButton17`,
     originalPrice: 849.99,
     newPrice: 749.99,
     descrip: `Go back to uh whenever cassettes were popular with these socks.`,
-    style: `Pattern`,
-    material: `Polyester`,
-    availability: `Out of stock`,
+    style: `pattern`,
+    material: `polyester`,
+    availability: `out of stock`,
     small: `s17`,
     medium: `m17`,
     large: `l17`,
 },{ //product 18
     image: `colourful-dots.jpg`,
     name: `Colourful dots`,
-    buttonId: `faveButton18`,
+    // buttonId: `faveButton18`,
     originalPrice: 19.99,
     newPrice: 18.99,
     descrip: `Brighten up your office with these socks.`,
-    style: `Dots`,
-    material: `Cotton`,
-    availability: `In stock`,
+    style: `dots`,
+    material: `cotton`,
+    availability: `in stock`,
     small: `s18`,
     medium: `m18`,
     large: `l18`,
 },{ //product 19
     image: `cactus.jpg`,
     name: `Cactus cowboy`,
-    buttonId: `faveButton19`,
+    // buttonId: `faveButton19`,
     originalPrice: 749.99,
     newPrice: 699.99,
     descrip: `Yeehaw.`,
-    style: `Pattern`,
-    material: `Polyester`,
-    availability: `Low stock`,
+    style: `pattern`,
+    material: `polyester`,
+    availability: `low stock`,
     small: `s19`,
     medium: `m19`,
     large: `l19`,
@@ -268,7 +268,54 @@ const products = [
 
 
 //Functionality
+
+//Filter style
+function isAFilterStyle (product) {
+    if (this == 'style') {
+        return true;
+    } else if (product.style == this){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//Filter material
+function isAFilterMaterial (product) {
+    if (this == 'material') {
+        return true;
+    } else if (product.material == this){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//Filter availability
+function isAFilterAvailability (product) {
+    if (this == `availability`) {
+        return true;
+    } else if (product.availability == this) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function loadProducts(product) {
+
+//Product availability coloured classes
+let availabilityclass; 
+
+if (product.availability == `in stock`) {
+    availabilityclass = `class="stock-tag stock-available"`;
+} else if (product.availability == `low stock`) {
+    availabilityclass = `class="stock-tag stock-low"`;
+} else {
+    availabilityclass = `class="stock-tag stock-none"`;
+}
+
+
 
      
 
@@ -278,7 +325,7 @@ function loadProducts(product) {
                     <p class="sale-tag">sale</p>
                 </header>
                 <h3 class="product-name">${product.name}</h3>
-                <button id="${product.buttonId}" class="fave-button" type="button"> <!-- id="faveButton" -->
+                <button id="faveButton" class="fave-button" type="button"> <!-- id="faveButton" -->
                         <span id="faveColour" class="fas fa-heart unfavourited"></span> <!-- id="faveColour" -->
                 </button>
                 <data class="product-price" value="39"><del>$${product.originalPrice}</del> <ins>$${product.newPrice}</ins></data>
@@ -286,7 +333,7 @@ function loadProducts(product) {
                 <ul class="product-info">
                 <li><strong>Style: </strong>${product.style}</li> 
                 <li><strong>Material: </strong>${product.material}</li>
-                <li><strong>Availability: </strong><em>${product.availability}</em></li>
+                <li><strong>Availability: </strong><em ${availabilityclass}>${product.availability}</em></li>
                 </ul>
                 <form class="product-buy">
                 <fieldset id="prodSize" class="product-size">
@@ -351,6 +398,25 @@ document.getElementById(`toggleViewClose`).addEventListener(`click`, toggleMenuC
 
 
 
+
+
+//FILTERS
+
+function showFilteredStyles (event) {
+    const selectedFilterStyle = event.target.value;
+    renderProducts(products.filter(isAFilterStyle, selectedFilterStyle));
+}
+
+function showFilteredMaterials (event) {
+    const selectedFilterMaterial = event.target.value;
+    renderProducts(products.filter(isAFilterMaterial, selectedFilterMaterial));
+}
+
+function showFilteredAvailability (event) {
+    const selectedFilterAvailability = event.target.value;
+    renderProducts(products.filter(isAFilterAvailability, selectedFilterAvailability));
+}
+
 // Toggle filters OPEN
 // function toggleFiltersOpen() {
 //     // console.log(`click`);
@@ -370,7 +436,7 @@ document.getElementById(`toggleViewClose`).addEventListener(`click`, toggleMenuC
 function addToFavourites() {
     document.getElementById(`faveColour`).classList.toggle(`favourited`);
 }
-document.getElementById(`${product.buttonId}`).addEventListener(`click`, addToFavourites);
+document.getElementById(`faveButton`).addEventListener(`click`, addToFavourites);
 //NEED TO PUT IDS BACK IN THE HTML
 // Add product to favourites
 // function addToFavourites() {
@@ -384,9 +450,11 @@ document.getElementById(`${product.buttonId}`).addEventListener(`click`, addToFa
 
 
 
+document.getElementById('filterStyle').addEventListener('change', showFilteredStyles);
 
+document.getElementById('filterMaterial').addEventListener('change', showFilteredMaterials);
 
-
+document.getElementById('filterAvailability').addEventListener('change', showFilteredAvailability);
 
 
 
